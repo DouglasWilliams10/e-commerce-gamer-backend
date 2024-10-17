ab8101e96dee1461ae1b2b0ae4feddd7b4e4ed6f
Douglas Porto <douglaswilliams.develop@outlook.com>
2024-10-17 17:01:47 -0300

    Feat: Adcionando o Get products e o Get products/specification/emphasis

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Products, products } from 'src/core';
 
-@Controller('product')
-export class ProductController {}
+@Controller('products')
+export class ProductController {
+    @Get()
+    getProducts(): Products[] {
+        return products;
+    }
+    @Get('specifications')
+    getSpecifications(): Products[] {
+        return products.map((products) => ({
+            ...products,
+            specifications: { emphasis: products.specifications.emphasis },
+        }));
+    }
+}
+
