6314332cde467b141874e5c4b96c6d2c6b0334a4
Douglas Porto <douglaswilliams.develop@outlook.com>
2024-11-04 16:06:59 -0300

    Fix: Corrigido o erro de busca do produto por id

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { resolve } from 'path';
-import { Products, products } from 'src/core';
+import { Controller, Get, Param } from '@nestjs/common';
+// import { resolve } from 'path';
+import { Product, products } from 'src/core';
 
-@Controller('products')
+@Controller('product')
 export class ProductController {
     @Get()
-    async getProducts(): Promise<Products[]> {
+    async getProducts(): Promise<Product[]> {
         // await this.esperarSegundos(2); // Chamada aqui a função que faz esperar 2 segundos antes de renderizar a função
         return products;
     }
     @Get('specifications')
-    getSpecifications(): Products[] {
+    getSpecifications(): Product[] {
         return products.map((products) => ({
             ...products,
             specifications: { emphasis: products.specifications.emphasis },
         }));
+    }
+    @Get(':id')
+    async getProductId(@Param('id') id:string): Promise<Product | null> {
+        const product = products.find((product) => product.id === +id);
+        return product ?? null;
     }
 
     // Função que faz esperar segundos antes de renderizar a lista de produtos.
